854a99bafdd78e4b95c2ade79c63e8fc9a6cfb67
Guudz <Kilian1702@outlook.com>
2023-01-24 03:07:05 +0100

    Initial commit

---
+++ data/vehnames.lua
@@ -0,0 +1,17 @@
+--[[
+
+Made by Dunning
+Do not Distribute
+
+]]--
+
+Citizen.CreateThread(function()
+
+-- Added Vehicles
+AddTextEntry("MULTI14TAH", "'14 Multi-Purpose Tahoe PPV (B/B)")
+AddTextEntry("MULTI14TAH2", "'14 Multi-Purpose Tahoe PPV (R/B)")
+
+-- Vehicle Brands
+AddTextEntry("CHEVY", "Chevrolet")
+
+end)
---
+++ stream/Vehicles/14tah/mods/14tah.lua
@@ -0,0 +1,51 @@
+Citizen.CreateThread(function()
+
+-- 200_TAH_modkit
+AddTextEntry("TAH01_RAIN_GUARDS", "Add Rain Guards")
+
+AddTextEntry("TAH01_REAR_1", "Remove Tow Hitch")
+AddTextEntry("TAH01_REAR_2", "Add Bike Rack (w/o bike)")
+AddTextEntry("TAH01_REAR_3", "Add Bike Rack (w/ bike)")
+
+AddTextEntry("TAH01_BADGES_1", "Remove Chevy Badges")
+
+AddTextEntry("TAH01_FRONT_1", "Add Upper Wrap")
+AddTextEntry("TAH01_FRONT_2", "Add Lower Wrap")
+AddTextEntry("TAH01_FRONT_3", "Add Upper/Lower Wrap")
+
+AddTextEntry("TAH01_FPLATE_1", "Add Plate Holder")
+AddTextEntry("TAH01_FPLATE_2", "Remove Front Plate")
+
+AddTextEntry("TAH01_RPLATE_1", "Remove Rear Plate")
+
+AddTextEntry("TAH01_INSERT_1", "Add Rear Prisoner Insert")
+AddTextEntry("TAH01_INSERT_2", "Add Rear Canine Insert (w/o dog)")
+AddTextEntry("TAH01_INSERT_3", "Add Rear Canine Insert (w/ dog)")
+
+AddTextEntry("TAH01_ALPR_1", "Add Roof Mounted ALPRs")
+
+AddTextEntry("TAH01_ROOFANT_1", "Add Roof Antennas Option 1")
+AddTextEntry("TAH01_ROOFANT_2", "Add Roof Antennas Option 2")
+AddTextEntry("TAH01_ROOFANT_3", "Add Roof Antennas Option 3")
+
+AddTextEntry("TAH01_INT_REM1", "Remove Laptop")
+AddTextEntry("TAH01_INT_REM2", "Remove Partition")
+AddTextEntry("TAH01_INT_REM3", "Remove Console")
+AddTextEntry("TAH01_INT_REM4", "Remove Radar System")
+
+AddTextEntry("TAH01_SPTLGHTS_1", "Add Spotlight Option 1")
+AddTextEntry("TAH01_SPTLGHTS_2", "Add Spotlight Option 2")
+AddTextEntry("TAH01_SPTLGHTS_3", "Add Spotlight Option 3")
+AddTextEntry("TAH01_SPTLGHTS_4", "Add Spotlight Option 4")
+
+AddTextEntry("TAH01_SEATS_1", "Remove Leather Seats")
+AddTextEntry("TAH01_SEATS_2", "Add Plastic Seat Insert")
+
+AddTextEntry("TAH01_SPEAKER_1", "Add Whelen Siren Speakers")
+AddTextEntry("TAH01_SPEAKER_2", "Add Lamphus Siren Speakers")
+AddTextEntry("TAH01_SPEAKER_3", "Add Roo Siren Speakers")
+
+AddTextEntry("TAH01_ORGANISER_1", "Add Seat Organiser Option 1")
+AddTextEntry("TAH01_ORGANISER_2", "Add Seat Organiser Option 2")
+
+end)
